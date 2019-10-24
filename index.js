f4565d3d1160e55d4b02f94647b727c59f0958f1
calebredd <caleb.redd@yahoo.com>
2019-10-24 15:30:13 -0700

    Set up backbone server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express=require('express');
+const server=express();
+
+server.get("/",(req,res)=>{
+  res.send("Hello from express")
+})
+
+server.listen(5000, ()=>
+console.log("Server Running on https://localhost:5000"));
