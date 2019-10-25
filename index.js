c2d96b2140f90dafa12a85b309910d0f1b26e044
Caleb Redd <caleb.redd@yahoo.com>
2019-10-24 19:50:08 -0700

    get request for users complete

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 // implement your API here
 
 const express = require("express");
-
+const Users = require("./data/db.js");
 const server = express();
+
+server.use(express.json());
+
 const hobbits = [
-  { id: 1, name: 'Bilbo Baggins' },
-  { id: 2, name: 'Frodo Baggins' },
-  { id: 3, name: 'Samwise Gamgee' }
-]
+  { id: 1, name: "Bilbo Baggins" },
+  { id: 2, name: "Frodo Baggins" },
+  { id: 3, name: "Samwise Gamgee" }
+];
+server.get("/api/hobbits", (req, res) => {
+  res.status(200).json(hobbits);
+});
+
 server.get("/", (req, res) => {
   res.send("Hello from MiddleEarth!");
 });
 
-server.get("/hobbits", (req, res) => {
-  // req.match.params.id
-  res.status(200).json(hobbits);
+server.get("/api/users", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+      // console.log(users);
+    })
+    .catch(err => {
+      res.status(500).json({errorMessage:'The users information could not be retrieved.'});
+      // console.log(err);
+    });
 });
 
-
 server.listen(5000, () =>
-  console.log("Adventures from Middle Earth await you at https://localhost:5000!")
+  console.log(
+    "Adventures from Middle Earth await you at https://localhost:5000!"
+  )
 );
