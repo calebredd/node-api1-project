1aaf0b7666687ee52f59ac3163a67973653629bf
Caleb Redd <caleb.redd@yahoo.com>
2019-10-24 19:02:01 -0700

    Added index.js file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+
+const express = require("express");
+
+const server = express();
+const hobbits = [
+  { id: 1, name: 'Bilbo Baggins' },
+  { id: 2, name: 'Frodo Baggins' },
+  { id: 3, name: 'Samwise Gamgee' }
+]
+server.get("/", (req, res) => {
+  res.send("Hello from MiddleEarth!");
+});
+
+server.get("/hobbits", (req, res) => {
+  // req.match.params.id
+  res.status(200).json(hobbits);
+});
+
+
+server.listen(5000, () =>
+  console.log("Adventures from Middle Earth await you at https://localhost:5000!")
+);
