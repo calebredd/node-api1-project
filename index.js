e95dc976e420ac85573698fb538bc6b160dbe1aa
Caleb Redd <caleb.redd@yahoo.com>
2019-10-26 16:22:07 -0700

    CRUD API funcitonality complete.

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ const server = express();
 server.use(express.json());
 
 const hobbits = [
-  { id: 1, name: "Bilbo Baggins" },
-  { id: 2, name: "Frodo Baggins" },
-  { id: 3, name: "Samwise Gamgee" }
+  {
+    name: "Bilbo Baggins",
+    bio: "The first hobbit to leave the shire"
+  },
+  {
+    name: "Frodo Baggins",
+    bio: "The ring bearer"
+  },
+  {
+    name: "Samwise Gamgee",
+    bio: "Gardener and poet. Married to Rose Cotton"
+  }
 ];
-server.get("/api/hobbits", (req, res) => {
-  res.status(200).json(hobbits);
-});
-
 server.get("/", (req, res) => {
   res.send("Hello from MiddleEarth!");
+});
+server.get("/api/hobbits", (req, res) => {
+  res.status(200).json(hobbits);
 });
 
 server.get("/api/users", (req, res) => {
@@ -26,8 +34,62 @@ server.get("/api/users", (req, res) => {
       // console.log(users);
     })
     .catch(err => {
-      res.status(500).json({errorMessage:'The users information could not be retrieved.'});
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
       // console.log(err);
+    });
+});
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json(err, { errorMessage: "That user could not be found." });
+    });
+});
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  // console.log(name);
+  // console.log(bio);
+  if (!name || !bio) {
+    res.status(400).send("A Name and Bio must be provided to add a user.");
+  } else {
+    Users.insert({ name: name, bio: bio })
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({ errorMessage: "Unable to add User." });
+      });
+  }
+});
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+  // console.log(name);
+  // console.log(bio);
+  if (!name || !bio) {
+    res.status(400).send("A Name and Bio must be provided to update a user.");
+  } else {
+    Users.update(req.params.id,{ name: name, bio: bio })
+      .then(user => {
+        res.status(202).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({ errorMessage: "Unable to add User." });
+      });
+  }
+});
+server.delete("/api/users/:id", (req, res) => {
+  Users.remove(req.params.id)
+    .then(user => {
+      res.status(202).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Unable to remove user." });
     });
 });
 
